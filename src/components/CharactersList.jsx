f876663f73fc1b5eaf09f456684f49ce5347b81c
alexandre-girard <alexandre.girard@maif.fr>
2025-10-07 21:39:23 +0200

    style: 🎨 ✨ format code for better readability in CharactersList component

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router";
 
+/**
+ * Component that displays a list of characters
+ * @param {*} characters - List of characters 
+ */
 export default function CharactersList({ characters = [] }) {
   return (
     <ul id="characters">
       {characters.map((character) => (
-        <Link to={`/characters/${character.id}`} key={character.id}>
-          <li>{character.name}</li>
-        </Link>
+          <Link to={`/characters/${character.id}`} key={character.id}>
+            <li>{ character.name }</li>
+          </Link>
       ))}
     </ul>
   );
