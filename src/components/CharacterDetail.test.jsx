f8e092da0589d0ff73feeb00c3699cb13bf1b061
alexandre-girard <alexandre.girard@maif.fr>
2025-10-08 11:36:15 +0200

    fix: 🐛 update CharacterDetail tests to use dynamic character properties

--- src/components/CharacterDetail.test.jsx
+++ src/components/CharacterDetail.test.jsx
@@ -27,15 +27,41 @@ describe('CharacterDetail', () => {
         };
         
         render(<CharacterDetail character={character} />);
-        const nameElement = screen.getByText('Spider-Man');
+        const nameElement = screen.getByText(character.name);
         expect(nameElement).toBeInTheDocument();
 
         const descriptionElement = screen.getByText(character.description);
         expect(descriptionElement).toBeInTheDocument();
 
-        const imageElement = screen.getByAltText(character.name);
+        const imageElement = screen.getByRole('img', { name: character.name });
         expect(imageElement).toBeInTheDocument();
         expect(imageElement).toHaveAttribute('src', 'https://example.com/spiderman/standard_large.jpg');
+
+        const modifiedElement = screen.getByText(character.modified);
+        expect(modifiedElement).toBeInTheDocument();
+
+        // uncomment to see the full DOM output
+        // screen.debug()
+    });
+
+    test('renders the character detail correctly when no thumbnail is provided', () => {
+        const character = {
+            id: '1',
+            name: 'Spider-Man',
+            description: 'A superhero with spider-like abilities.',
+            modified: '2014-01-13T14:48:32-0500',
+        };
+        
+        render(<CharacterDetail character={character} />);
+        const nameElement = screen.getByText(character.name);
+
+        expect(nameElement).toBeInTheDocument();
+
+        const descriptionElement = screen.getByText(character.description);
+        expect(descriptionElement).toBeInTheDocument();
+
+        const imageElements = screen.queryAllByRole('img');
+        expect(imageElements).toHaveLength(0); // No image should be rendered
 
         const modifiedElement = screen.getByText(character.modified);
         expect(modifiedElement).toBeInTheDocument();
