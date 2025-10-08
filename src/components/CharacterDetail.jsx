843daddd07dc064c3572bcb9404c3a0d6808efc1
alexandre-girard <alexandre.girard@maif.fr>
2025-10-08 07:50:25 +0200

    fix: 🐛 refactor CharacterDetail component for improved readability

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -2,24 +2,26 @@
  * Component to render the details of a character
  * @param {*} character - Character object 
  */
+
 function CharacterDetail({ character = {} }) {
-    return !character.name ? (
-        // if character name is not provided, then display "No character"
-        <div>No character</div>
-    ) : (
-        // else display the character details
-        <div>
-            <h2>{character.name}</h2>
-            {character.thumbnail && (
-                <img
-                    src={`${character.thumbnail.path}/standard_large.${character.thumbnail.extension}`}
-                    alt={character.name}
-                />
-            )}
-            <p>{character.description}</p>
-            <p>{character.modified}</p>
-        </div>
-    );
+    if (character.name) {
+        // display the character details
+        return (
+            <div>
+                <h2>{character.name}</h2>
+                {character.thumbnail && (
+                    <img
+                        src={`${character.thumbnail.path}/standard_large.${character.thumbnail.extension}`}
+                        alt={character.name}
+                    />
+                )}
+                <p>{character.description}</p>
+                <p>{character.modified}</p>
+            </div>
+        );
+    }
+    // if character name is not provided, then display "No character"
+    return <div>No character</div>;
 }
 
 export default CharacterDetail;
