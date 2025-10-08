028f467c0722b82fa9585f4d1a21acd64cb16cbd
alexandre-girard <alexandre.girard@maif.fr>
2025-10-08 07:20:26 +0200

    test: 🚨 🧪 add unit tests for CharactersList component

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -7,11 +7,11 @@ import { Link } from "react-router";
 export default function CharactersList({ characters = [] }) {
   return (
     <ul id="characters">
-      {characters.map((character) => (
+      { characters.map((character) => (
           <Link to={`/characters/${character.id}`} key={character.id}>
             <li>{ character.name }</li>
           </Link>
-      ))}
+      ))} 
     </ul>
   );
 }
